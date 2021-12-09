e8897e0cd18742f9d9d5d2408b211cf158edc6af
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-09 08:41:25 -0500

    Operador de incremento y decremento

--- aprendiendo-php/06-operadores/index.php
+++ aprendiendo-php/06-operadores/index.php
@@ -12,4 +12,24 @@ echo 'Multiplicación: '.($numero1 * $numero2).'<br>';
 echo 'División: '.($numero1 / $numero2).'<br>';
 echo 'Resto: '.($numero1 % $numero2).'<br>';
 
+//Operadores de incremento y decremento
+
+$year = 2019;
+
+//Incremento
+$year++;
+
+//Decremento
+$year--;
+
+//Pre-incremento
+// $year = 1 + $year;
+++$year;
+
+//Pre-drecemento
+// $year = 1 - $year;
+--$year;
+
+echo '<h1>'.$year.'</h1>';
+
 ?>
