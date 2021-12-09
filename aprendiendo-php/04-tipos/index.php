4660d6aa60361cc0a46bb6870dfde3d7be46c465
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-08 19:04:47 -0500

    Tipos de datos php

---
+++ aprendiendo-php/04-tipos/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+  Tipos de Datos:
+  Entero (int / integer) = 99
+  Coma flotante / decimales (float / double) = 3.45
+  Cadena (string) = "Hola yo soy un string"
+  Boleando (bool) = 1 o 0 (true o false)
+  null = ningun contenido adentro
+  Array (Colección de datos)
+  Objetos
+ */
+
+ $numero = 100;
+ $decimal = 27.9;
+ $texto = "Soy un texto";
+ $verdadero = true;
+ $nula = null;
+
+ echo gettype($nula);
+
+?>
