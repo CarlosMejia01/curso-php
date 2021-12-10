53d8f57fcd71e3e66d3a2823639ee7d1677619c2
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-10 07:40:16 -0500

    Estructura elseif

--- aprendiendo-php/08-condicionales/index.php
+++ aprendiendo-php/08-condicionales/index.php
@@ -25,7 +25,7 @@ Operadores de comparación
 //Ejemplo 1
 $color = "verde";
 
-if($color == "rojo") {
+if ($color == "rojo") {
     echo "El color es rojo";
 } else {
     echo "El color no es rojo";
@@ -35,7 +35,7 @@ echo "<br>";
 
 //Ejemplo 2
 $year = 2019;
-if($year != 2019) {
+if ($year != 2019) {
     echo "Es un año diferente al 2019";
 } else {
     echo "Estamos 2019";
@@ -49,9 +49,9 @@ $continente = "Asia";
 $edad = 19;
 $mayoria_edad = 18;
 
-if($edad >= $mayoria_edad) {
+if ($edad >= $mayoria_edad) {
     echo "<h1> $nombre es mayor de edad </h1>";
-    if($continente == "Europa") {
+    if ($continente == "Europa") {
         echo "<h2> Y es de ciudad $ciudad </h2>";
     } else {
         echo "<h2> No es Europeo </h2>";
@@ -60,5 +60,44 @@ if($edad >= $mayoria_edad) {
     echo "<h2> $nombre No es mayor de edad </h2>";
 };
 
+echo "<br>";
+//Ejemplo 4
 
-?>
+$dia = 3;
+/*
+if ($dia == 1) {
+    echo "Es Lunes";
+} else {
+    if ($dia == 2) {
+        echo "Es Martes";
+    } else {
+        if ($dia == 3) {
+            echo "Es Miercoles";
+        } else {
+            if ($dia == 4) {
+                echo "Es Jueves";
+            } else {
+                if ($dia == 5) {
+                    echo "Es Viernes";
+                } else {
+                    echo "Fin de semana";
+                }
+            }
+        }
+    }
+}
+*/
+
+if ($dia == 1) {
+    echo "Lunes";
+} elseif ($dia == 2) {
+    echo "Martes";
+} elseif ($dia == 3) {
+    echo "Miercoles";
+} elseif ($dia == 4) {
+    echo "Jueves";
+} elseif ($dia == 5) {
+    echo "Viernes";
+} else {
+    echo "Fin de semana";
+}
