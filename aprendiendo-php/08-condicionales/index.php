efe08c5ded47e3d35b3762833efc2d6dcd52cfd1
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-10 07:34:12 -0500

    Ejemplo 3, operadores de asignación

--- aprendiendo-php/08-condicionales/index.php
+++ aprendiendo-php/08-condicionales/index.php
@@ -22,7 +22,7 @@ Operadores de comparación
     <= "menor o igual que"
     >= "mayor o igual que"
 */
-
+//Ejemplo 1
 $color = "verde";
 
 if($color == "rojo") {
@@ -32,11 +32,32 @@ if($color == "rojo") {
 }
 
 echo "<br>";
+
+//Ejemplo 2
 $year = 2019;
 if($year != 2019) {
     echo "Es un año diferente al 2019";
 } else {
     echo "Estamos 2019";
+};
+
+//Ejemplo 3
+
+$nombre = "Carlos Test";
+$ciudad = "Madrid";
+$continente = "Asia";
+$edad = 19;
+$mayoria_edad = 18;
+
+if($edad >= $mayoria_edad) {
+    echo "<h1> $nombre es mayor de edad </h1>";
+    if($continente == "Europa") {
+        echo "<h2> Y es de ciudad $ciudad </h2>";
+    } else {
+        echo "<h2> No es Europeo </h2>";
+    }
+} else {
+    echo "<h2> $nombre No es mayor de edad </h2>";
 };
 
 
