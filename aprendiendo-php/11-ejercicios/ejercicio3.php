7186b25d11c888ede8c19ff9eedfda46e609da1e
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-10 21:25:46 -0500

    Ejercicio 3

---
+++ aprendiendo-php/11-ejercicios/ejercicio3.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Ejercicio 3: Escribir un programa que imprima los cuadrados
+ * (un numero multiplacado por si mismo) de los 40 primeros numeros naturales
+ * Bucle While
+ */
+// $i =1;
+//  while($i<=40) {
+//     echo "El cuadrado de $i es = ".($i*$i)."<br>";
+//      $i++;
+//  }
+
+ for($i=1;$i<=40;$i++){
+    echo "El cuadrado de $i es = ".($i*$i)."<br>";
+ }
+
+
+?>
