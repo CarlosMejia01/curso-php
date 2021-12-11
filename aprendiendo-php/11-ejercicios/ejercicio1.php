ada7c76aa0d1f0a7394f85af382350f3cc361784
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-10 21:14:21 -0500

    Ejercicio 1

---
+++ aprendiendo-php/11-ejercicios/ejercicio1.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Crear dos variables "pais" y "continente" y mostrar su valor por pantalla
+ * comentario, que tipo de dato tienen
+ */
+
+$pais = "Paris"; //string
+$continente = "Europa"; //string
+
+
+echo "El pais es $pais y el continente es $continente";
+echo "<br>";
+var_dump($pais);
+echo "<br>";
+var_dump($continente);
+
+
+?>
