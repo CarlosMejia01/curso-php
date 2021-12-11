f665946ebe9ece871e8d9ef3b7c1d403aad44df8
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-10 20:40:58 -0500

    Bucle for

---
+++ aprendiendo-php/10-bucle-for/index.php
@@ -0,0 +1,36 @@
+<?php
+/*
+//For
+for(expresion contador, condicion, actualizando contador ) {
+    //Bloque
+}
+*/
+
+/*
+$resultado = 0;
+
+for($i = 0; $i <= 100; $i++) {
+    $resultado += $i;
+    echo "<p>$resultado</p>";
+} 
+
+
+echo "<h1> el resultado es: $resultado</h1>";
+
+*/
+
+if(isset($_GET['numero'])) {
+    //cambiar tipo de dato
+    $numero = (int) $_GET['numero'];
+} else {
+    $numero = 1;
+}
+
+echo "<h1>Tabla de multiplicar del número $numero</h1>";
+
+for($contador = 1;$contador <= 10;$contador++) {
+    echo "$numero x $contador = ".($numero*$contador)."<br/>";
+}
+
+
+?>
