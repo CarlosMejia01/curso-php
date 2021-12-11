da14cad59f79da4a0bcf752d6a1839919f9886d5
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-10 19:53:03 -0500

    Bucle Whule ejemplo

--- aprendiendo-php/09-bucles/index.php
+++ aprendiendo-php/09-bucles/index.php
@@ -21,5 +21,25 @@ while($num <= 100) {
     $num++;
 };
 
+echo "<hr>";
+//Ejemplo
+
+if(isset($_GET['numero'])) {
+    //cambiar tipo de dato
+    $numero = (int) $_GET['numero'];
+} else {
+    $numero = 1;
+}
+
+echo "<h1>Tabla de multiplicar del número $numero</h1>";
+
+$contador = 0;
+while($contador <= 10) {
+    echo "$numero x $contador = ".($numero*$contador)."<br/>";
+    $contador++;
+}
+
+var_dump($numero);
+
 
 ?>
