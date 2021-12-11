b00388db04b2274cc2ae5f032a0c4165e1a39884
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-11 18:40:18 -0500

    Ejemplo 2 funciones

--- aprendiendo-php/12-funciones/index.php
+++ aprendiendo-php/12-funciones/index.php
@@ -38,13 +38,43 @@ function tabla($numero) {
         echo "$numero x $i = ".($numero*$i)."<br>";
     };
 };
+/*
 if(isset($_GET['numero'])) {
     tabla($_GET['numero']);
 } else {
     echo "No hay número para sacar la tabla";
 }
+*/
 
+//Ejemplo 3
 
+function calculadora($numero1, $numero2, $negrita = false) {
+    //Conjunto de instrucciones a ejecutar
+    $suma = $numero1 + $numero2;
+    $resta = $numero1 - $numero2;
+    $multi = $numero1 * $numero2;
+    $division = $numero1 / $numero2;
+
+    if($negrita) {
+        echo "<h1>";
+    };
+
+    echo "Suma: $suma <br/>";
+    echo "Resta: $resta <br/>";
+    echo "Multiplicación: $multi <br/>";
+    echo "division: $division <br/>";
+    
+    if($negrita) {
+        echo "</h1>";
+    };
+    
+    
+    echo "<hr>";
+};
+
+calculadora(10, 30);
+calculadora(30, 5, true);
+calculadora(40, 20);
 
 
 ?>
