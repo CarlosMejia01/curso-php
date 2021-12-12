9bb62099ae64e06f5a566d14dc53dd764c4838c2
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-11 22:14:30 -0500

    Funciones internas

--- aprendiendo-php/12-funciones/predefinidas.php
+++ aprendiendo-php/12-funciones/predefinidas.php
@@ -22,5 +22,70 @@ echo "Número pi: ".pi();
 
 echo "<br/>";
 echo "Redondear: ".round(7.891234, 2);
+echo "<hr/>";
+
+//Más funciones generales
+echo "<br/>";
+echo gettype($nombre);
+
+//Detectar tipado
+echo "<br/>";
+if(is_string($nombre)) {
+    echo "La variable es string";
+};
+
+echo "<br/>";
+if(!is_float($nombre)) {
+    echo "La variable nombre no es un número con decimales";
+};
+
+//Comprobar si existe una variable
+echo "<br/>";
+if(isset($nombre)) {
+    echo "La variable existe";
+} else {
+    echo "La variable no existe";
+}
+
+//Limpiar espacios
+echo "<br/>";
+$frase = "     mi contenido     ";
+echo (trim($frase));
+
+//Eliminar variables / indices
+$year = 2020;
+unset($year);
+
+//Comprobar variable vacia
+$texto = "data";
+echo "<br>";
+if(empty($texto)) {
+    echo "La variable esta vacia";
+} else {
+    echo "La variable tiene contenido";
+};
+
+//Contar caracteres de una cadena
+echo "<br/>";
+$cadena = "12345";
+echo strlen($cadena);
+
+//Encontrar caracter
+echo "<br/>";
+$frase = "La vida es bella";
+echo strpos($frase, "i");
+
+//Reemplazar palabras de un string
+echo "<br/>";
+$frase = str_replace("vida", "moto", $frase);
+echo $frase;
+
+//Mayusculas a minusculas
+echo "<br>";
+echo strtolower($frase);
+echo "<br>";
+echo strtoupper($frase);
+
+
 
 ?>
