8f43d5e6552f8cede3bdf29d5cd8d2ec3ba2030f
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-11 21:20:58 -0500

    Funciones predefinidas

---
+++ aprendiendo-php/12-funciones/predefinidas.php
@@ -0,0 +1,26 @@
+<?php
+//Debuggear
+$nombre = "Charly One";
+
+var_dump($nombre);
+
+//Fechas
+echo "<br/>";
+echo date('d-m-y');
+echo "<br/>";
+echo time();
+
+
+//Matematicas
+echo "<br/>";
+echo "Raiz cuadrada de 10: ".sqrt(10);
+echo "<br/>";
+echo "Número aleatorio entre 10 y 40: ".rand(10, 40);
+
+echo "<br/>";
+echo "Número pi: ".pi();
+
+echo "<br/>";
+echo "Redondear: ".round(7.891234, 2);
+
+?>
