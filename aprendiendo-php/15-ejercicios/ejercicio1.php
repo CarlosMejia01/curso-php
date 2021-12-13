0a6b1e25ccca44a90080dbcdf53ae927d7c9bb31
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-12 20:43:43 -0500

    Prueba 1 arrays

--- aprendiendo-php/15-ejercicios/ejercicio1.php
+++ aprendiendo-php/15-ejercicios/ejercicio1.php
@@ -1,8 +1,37 @@
 <?php
 
 /**
- * Ejercicio 1. Hacer un programa en php que tenga un array con 8 numeros 
+ * Ejercicio 1. Hacer un programa en php que tenga un array con 8 numeros enteros
+ * y que haga lo siguiente:
+ * -Recorrerlo y mostrarlo
+ * -Ordernalo y mostrarlo
+ * -Mostrar su longitud
+ * Buscar algun elemento
  */
 
+
+ $numeros = [3,6,2,1,5,7,4,8];
+
+ //Recorrer y mostrar números
+
+ for($i = 0; $i < count($numeros); $i++) {
+    echo "Número ".$numeros[$i]."<br>";
+ };
+
+ //Ordernalo y mostrarlo
+ echo "<br>";
+ echo "Ordenado";
+ echo "<br>";
+
+ sort($numeros);
+ for($i = 0; $i < count($numeros); $i++) {
+    echo "Número ".$numeros[$i]."<br>";
+ };
+
+ echo "<br>";
+ echo "Longitud array números es: ".count($numeros);
+
+ echo "<br>";
+ echo "Buscar número 7, posición: ".array_search(7, $numeros);
 
 ?>
