8f987ef446b1de37bef074178bf74aedf2f3f263
Carlos Mejia <mrcarlitos007@gmail.com>
2021-12-19 19:05:36 -0500

    formularios

---
+++ aprendiendo-php/18-formularios/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formularios PHP y HTML</title>
+</head>
+<body>
+    <h1>Formulario</h1>
+    <form action="" method="" enctype="multipart/form-data">
+        <label for="nombre">Nombre: </label>
+        <p><input type="text" name="nombre"></p>
+        <label for="apellido">Apellido: </label>
+        <p><input type="text" name="apellido" placeholder="Ingresa tu apellido"></p>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
